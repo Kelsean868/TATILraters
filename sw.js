757ac9d7bb23ba49452dcf137a131d079c92948c
Kelsean868 <kyronmarchan@gmail.com>
2025-06-19 15:07:52 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,19 @@
 // This is the service worker script (sw.js)
 
 // Define a unique name for your cache. 
-// It's a good practice to include a version number.
-// If you update any of your site's files, you should increment this version number.
-const CACHE_NAME = 'tatil-raters-cache-v1.1';
+// Increment this version number when you update any of the cached files.
+const CACHE_NAME = 'tatil-rater-hub-cache-v1.0';
 
-// This is the list of all the files that make up your app's "shell".
-// We will cache all of these upon installation.
+// This is the complete list of all the files that make up your app.
+// The service worker will cache all of these upon installation.
 const URLS_TO_CACHE = [
-  '/',
-  'index.html',
-  'motor_rater.html',
-  'health_rater.html',
-  'motor_rater_data.js',
-  'health_rater_data.js',
-  'manifest.json',
+  './',
+  './index.html',
+  './motor_rater.html',
+  './health_rater.html',
+  './motor_rater_data.js',
+  './health_rater_data.js',
+  './manifest.json',
   'https://cdn.tailwindcss.com',
   'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&family=Source+Serif+Pro:wght@600;700&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
@@ -22,21 +21,22 @@ const URLS_TO_CACHE = [
 ];
 
 // --- INSTALL Event ---
-// This event is fired when the service worker is first installed.
+// Fired when the service worker is first installed.
 // We open our cache and add all the app shell files to it.
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache and caching app shell');
+        console.log('Opened cache and caching all app files');
         return cache.addAll(URLS_TO_CACHE);
       })
   );
+  self.skipWaiting();
 });
 
 // --- ACTIVATE Event ---
-// This event is fired when the service worker is activated.
-// This is a good place to clean up old caches to remove outdated files.
+// Fired when the service worker is activated.
+// This is where we clean up old, unused caches.
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
@@ -51,28 +51,22 @@ self.addEventListener('activate', event => {
       );
     })
   );
+  return self.clients.claim();
 });
 
 // --- FETCH Event ---
-// This event is fired for every single request the page makes (e.g., for CSS, images, data).
-// We'll intercept these requests and serve files from the cache if they are available.
+// Fired for every network request. We use a "Cache first, then network" strategy.
+// This makes the app load instantly from the cache if available.
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // If the request is in the cache, return the cached response.
+        // If we find a match in the cache, return it.
         if (response) {
           return response;
         }
-
-        // If the request is not in the cache, fetch it from the network.
-        return fetch(event.request).then(
-          networkResponse => {
-            // We can also cache new requests as they are made,
-            // but for this app, pre-caching is sufficient.
-            return networkResponse;
-          }
-        );
+        // Otherwise, fetch the request from the network.
+        return fetch(event.request);
       })
   );
 });
