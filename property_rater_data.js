bc45b345c7aaeee09666a48a4888962d328a95b8
Kelsean868 <kyronmarchan@gmail.com>
2025-06-23 10:22:00 -0400

    Update property_rater_data.js
    updated calculations

--- property_rater_data.js
+++ property_rater_data.js
@@ -4,10 +4,25 @@
 const PROPERTY_RATES = {
     homeowners: {
         building: {
-            group: 3.50,    // Rate per $1000 of value
-            individual: 3.75 // Rate per $1000 of value
+            group: 3.50,        // Rate per $1000 of value
+            individual: 3.75,   // Rate per $1000 of value
+            flood_prone: 5.00   // Increased rate per $1000 for flood-prone areas
         },
-        contents: 4.00      // Rate per $1000 of value
+        contents: {
+            general: 4.00,          // Rate per $1000 of value
+            electronics: 0.01,      // 1% of value
+            jewelry: {
+                on_premises: 0.015,     // 1.5% of value
+                trinidad_tobago: 0.02,  // 2% of value
+                worldwide: 0.025        // 2.5% of value
+            },
+            special_items: 0.01,    // 1% of value
+            all_risk: 0.03          // 3% of total contents value
+        },
+        excess: {
+            standard_flood: 2500,
+            increased_flood: 5000
+        }
     },
     commercial: {
         fire: 3.50          // Rate per $1000 of value
